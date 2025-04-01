8134a9b673fca3f38b9a21e3f674157d77a82b18
Renvel19 <af.renteria2@uniandes.edu.co>
2025-03-31 19:41:58 -0500

    configure beanstalk deployment

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from src import create_app
 
 load_dotenv()
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 3000)))
+    application.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,12 @@ from datetime import datetime, timezone
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI", "sqlite:///blacklists.db")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_PORT = os.getenv("DB_PORT")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI',f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     JWTManager(app)
