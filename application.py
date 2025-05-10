3691200a8604df6c6541aa1b5f66490394266758
Renvel19 <af.renteria2@uniandes.edu.co>
2025-05-10 11:03:56 -0500

    add commit to blue green

--- application.py
+++ application.py
@@ -1,11 +1,17 @@
 import os
 
 from dotenv import load_dotenv
+from flask import jsonify
+
 from src import create_app
 
 load_dotenv()
 
 application = create_app()
+
+@application.route("/")
+def index():
+    return jsonify({"message": "Prueba despliegue Blue/green"})
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", port=5000, debug=True)
